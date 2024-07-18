5742fb334c683332f30b9c70dc7396cecf96b52c
Martin Robinson <mrobinson@igalia.com>
2024-07-18 09:49:00 +0200

    Switch from `dirs-next` to `dirs` and release a new version
    `dirs-next` is now the abandoned crate, so switch to `dirs`.

--- src/sources/fs.rs
+++ src/sources/fs.rs
@@ -20,7 +20,7 @@ use std::path::{Path, PathBuf};
 use walkdir::WalkDir;
 
 #[cfg(not(any(target_os = "android", target_family = "windows", target_env = "ohos")))]
-use dirs_next;
+use dirs;
 #[cfg(target_family = "windows")]
 use std::ffi::OsString;
 #[cfg(target_family = "windows")]
@@ -203,7 +203,7 @@ fn default_font_directories() -> Vec<PathBuf> {
         PathBuf::from("/Library/Fonts"),
         PathBuf::from("/Network/Library/Fonts"),
     ];
-    if let Some(mut path) = dirs_next::home_dir() {
+    if let Some(mut path) = dirs::home_dir() {
         path.push("Library");
         path.push("Fonts");
         directories.push(path);
@@ -224,11 +224,11 @@ fn default_font_directories() -> Vec<PathBuf> {
         PathBuf::from("/var/run/host/usr/share/fonts"), // Flatpak specific
         PathBuf::from("/var/run/host/usr/local/share/fonts"),
     ];
-    if let Some(path) = dirs_next::home_dir() {
+    if let Some(path) = dirs::home_dir() {
         directories.push(path.join(".fonts")); // ~/.fonts is deprecated
         directories.push(path.join("local").join("share").join("fonts")); // Flatpak specific
     }
-    if let Some(mut path) = dirs_next::data_dir() {
+    if let Some(mut path) = dirs::data_dir() {
         path.push("fonts");
         directories.push(path);
     }
